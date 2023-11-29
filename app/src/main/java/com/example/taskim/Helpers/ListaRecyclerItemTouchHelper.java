5e17ea92b292cb24f5d6f938d6d59500423eb91c
joaopedropsilva <83987930+joaopedropsilva@users.noreply.github.com>
2023-11-28 21:25:35 -0300

    Adição de comentários nas classes de helpers

--- app/src/main/java/com/example/taskim/Helpers/ListaRecyclerItemTouchHelper.java
+++ app/src/main/java/com/example/taskim/Helpers/ListaRecyclerItemTouchHelper.java
@@ -19,9 +19,15 @@ import com.example.taskim.ListagemListasActivity;
 import com.example.taskim.R;
 import com.example.taskim.Fragments.DialogEditText;
 
+
+// Classe de helper de ItemTouchHelper.SimpleCallback
+// responsável pelas as ações de deslizar para os lados
+// um item do tipo lista de tarefas
 public class ListaRecyclerItemTouchHelper extends ItemTouchHelper.SimpleCallback {
     private final ListasAdapter listasAdapter;
 
+    // Definição do construtor com o adapter
+    // responsável pelas listas de tarefas
     public ListaRecyclerItemTouchHelper(ListasAdapter listasAdapter) {
         super(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT);
         this.listasAdapter = listasAdapter;
@@ -34,10 +40,16 @@ public class ListaRecyclerItemTouchHelper extends ItemTouchHelper.SimpleCallback
             @NonNull RecyclerView.ViewHolder target
     ) { return false; }
 
+    // Implementação da rotina de execução quando um usuário
+    // arrasta uma lista de tarefas para algum lado
     @Override
     public void onSwiped(@NonNull final RecyclerView.ViewHolder viewHolder, int direction) {
+        // Recuperação da posição da lista de tarefas em questão
         final int listaIndex = viewHolder.getAdapterPosition();
 
+        // Ação tomada ao arrastar para a
+        // esquerda: abertura de um Dialog
+        // para a deleção do item em questão
         if (direction == ItemTouchHelper.LEFT) {
             AlertDialog.Builder builder = new AlertDialog.Builder(listasAdapter.getContext());
             builder.setMessage("Deletar lista?");
@@ -48,29 +60,45 @@ public class ListaRecyclerItemTouchHelper extends ItemTouchHelper.SimpleCallback
 
             AlertDialog dialogDeleteLista = builder.create();
             dialogDeleteLista.show();
-        } else {
+        }
+        // Ação tomada ao arrastar para a
+        // direita: abertura do DialogEditText
+        // para a edição do item em questão
+        else {
+            // Busca pela lista em questão na listagem
             Lista lista = listasAdapter.getListagemListas().get(listaIndex);
+
+            // Criação de um bundle com as informações
+            // da operação de edição
             Bundle listaInformartion = new Bundle();
             listaInformartion.putInt("id", lista.getId());
             listaInformartion.putString("nome", lista.getNome());
             listaInformartion.putString("operation", "edit");
 
+            // Criação do DialogEditText de edição
             DialogEditText dialogEditLista = new DialogEditText(
                     "Editar nome da lista",
                      "Salvar",
                     "Cancelar");
+
+            // Passagem dos argumentos para o dialog
             dialogEditLista.setArguments(listaInformartion);
 
+            // Recuperação da atividade em questão
             ListagemListasActivity activity = (ListagemListasActivity) listasAdapter.getContext();
+
+            // Função de abertura do Dialog de edição
             dialogEditLista.show(activity.getSupportFragmentManager(), DialogEditText.TAG);
         }
     }
 
+    // Função responsável pelo comportamento da View
+    // de lista de tarefas quando ela é arrastada
     @Override
     public void onChildDraw(
-            Canvas canvas,
-            RecyclerView recyclerView,
-            RecyclerView.ViewHolder viewHolder,
+            @NonNull Canvas canvas,
+            @NonNull RecyclerView recyclerView,
+            @NonNull RecyclerView.ViewHolder viewHolder,
             float dX,
             float dY,
             int actionState,
@@ -78,24 +106,45 @@ public class ListaRecyclerItemTouchHelper extends ItemTouchHelper.SimpleCallback
     ) {
         super.onChildDraw(canvas, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
 
+        // Declaração de recursos utilizados
+        // na estilização dos elementos em
+        // tela durante a ação
         Drawable swipeIcon;
         ColorDrawable background;
 
+        // Recuperação da view em qestão
         View itemView = viewHolder.itemView;
         int backgroundCornerOffset = 20;
 
+        // Deslocamento horizontal causado
+        // pelo usuário é maior que zero, se
+        // sim: exibição de um ícone de edição
+        // com a cor principal como fundo
         if (dX > 0) {
-            swipeIcon = ContextCompat.getDrawable(listasAdapter.getContext(), R.drawable.edit_icon);
-            background = new ColorDrawable(ContextCompat.getColor(listasAdapter.getContext(), R.color.colorPrimary));
-        } else {
-            swipeIcon = ContextCompat.getDrawable(listasAdapter.getContext(), R.drawable.delete_icon);
+            swipeIcon = ContextCompat.getDrawable(
+                    listasAdapter.getContext(), R.drawable.edit_icon);
+            background = new ColorDrawable(
+                    ContextCompat.getColor(listasAdapter.getContext(), R.color.colorPrimary));
+        }
+        // Caso contrário é exibido um ícone
+        // de deleção do item em questão
+        // com o fundo em vermelho
+        else {
+            swipeIcon = ContextCompat.getDrawable(
+                    listasAdapter.getContext(), R.drawable.delete_icon);
             background = new ColorDrawable(Color.RED);
         }
 
-            int swipeIconMargin = (itemView.getHeight() - swipeIcon.getIntrinsicHeight()) / 2;
-        int swipeIconTop = itemView.getTop() + (itemView.getHeight() - swipeIcon.getIntrinsicHeight()) / 2;
+        // Definição de variáveis de posicionamento do ícone
+        int swipeIconMargin = (itemView.getHeight() - swipeIcon.getIntrinsicHeight()) / 2;
+        int swipeIconTop = itemView.getTop() +
+                (itemView.getHeight() - swipeIcon.getIntrinsicHeight()) / 2;
         int swipeIconBottom = swipeIconTop + swipeIcon.getIntrinsicHeight();
 
+        // Deslocamento horizontal causado
+        // pelo usuário é maior que zero, se
+        // sim: definição de determinada aparência,
+        // posicionamento e limites para ícone/fundo
         if (dX > 0) {
             int swipeIconLeft = itemView.getLeft() + swipeIconMargin;
             int swipeIconRight = itemView.getLeft() + swipeIconMargin + swipeIcon.getIntrinsicHeight();
@@ -106,7 +155,12 @@ public class ListaRecyclerItemTouchHelper extends ItemTouchHelper.SimpleCallback
                     itemView.getTop(),
                     itemView.getLeft() + ((int) dX) + backgroundCornerOffset,
                     itemView.getBottom());
-        } else if (dX < 0) {
+        }
+        // Deslocamento horizontal causado
+        // pelo usuário é menor que zero, se
+        // sim: definição de outra aparência,
+        // posicionamento e limites para ícone/fundo
+        else if (dX < 0) {
             int swipeIconLeft = itemView.getRight() - swipeIconMargin - swipeIcon.getIntrinsicWidth();
             int swipeIconRight = itemView.getRight() - swipeIconMargin;
 
@@ -116,10 +170,17 @@ public class ListaRecyclerItemTouchHelper extends ItemTouchHelper.SimpleCallback
                     itemView.getTop(),
                     itemView.getRight(),
                     itemView.getBottom());
-        } else {
+        }
+        // Deslocamento horizontal causado
+        // pelo usuário é igual a zero, se
+        // sim: definição do fundo
+        // com dimensões nulas
+        else {
             background.setBounds(0, 0, 0, 0);
         }
 
+        // Desenho do ícone e fundo
+        // configurados na tela
         background.draw(canvas);
         swipeIcon.draw(canvas);
     }
--- app/src/main/java/com/example/taskim/Helpers/TarefaRecyclerItemTouchHelper.java
+++ app/src/main/java/com/example/taskim/Helpers/TarefaRecyclerItemTouchHelper.java
@@ -1,7 +1,6 @@
 package com.example.taskim.Helpers;
 
 import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
@@ -9,7 +8,6 @@ import android.graphics.drawable.Drawable;
 import android.view.View;
 
 import androidx.annotation.NonNull;
-import androidx.coordinatorlayout.widget.CoordinatorLayout;
 import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.RecyclerView;
@@ -17,23 +15,36 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.taskim.Adapters.TarefaAdapter;
 import com.example.taskim.R;
 
+// Classe de helper de ItemTouchHelper.SimpleCallback
+// responsável pelas as ações de deslizar para os lados
+// um item do tipo tarefa
 public class TarefaRecyclerItemTouchHelper extends ItemTouchHelper.SimpleCallback {
-    private TarefaAdapter tarefaAdapter;
+    private final TarefaAdapter tarefaAdapter;
 
+    // Definição do construtor com o adapter
+    // responsável pelas tarefas
     public TarefaRecyclerItemTouchHelper(TarefaAdapter tarefaAdapter) {
         super(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT);
         this.tarefaAdapter = tarefaAdapter;
     }
 
     @Override
-    public boolean onMove(@NonNull RecyclerView rvListaTarefa, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
-        return false;
-    }
+    public boolean onMove(
+            @NonNull RecyclerView rvListaTarefa,
+            @NonNull RecyclerView.ViewHolder viewHolder,
+            @NonNull RecyclerView.ViewHolder target
+    ) { return false; }
 
+    // Implementação da rotina de execução quando um
+    // usuário arrasta uma tarefa para algum lado
     @Override
     public void onSwiped(@NonNull final RecyclerView.ViewHolder viewHolder, int direction) {
+        // Recuperação da posição da tarefa em questão
         final int tarefaIndex = viewHolder.getAdapterPosition();
 
+        // Ação tomada ao arrastar para a
+        // esquerda: abertura de um Dialog
+        // para a deleção do item em questão
         if (direction == ItemTouchHelper.LEFT) {
             AlertDialog.Builder confirmDialogBuilder = new AlertDialog.Builder(tarefaAdapter.getContext());
             confirmDialogBuilder.setMessage("Deletar tarefa?");
@@ -44,16 +55,23 @@ public class TarefaRecyclerItemTouchHelper extends ItemTouchHelper.SimpleCallbac
 
             AlertDialog confirmDialog = confirmDialogBuilder.create();
             confirmDialog.show();
-        } else {
+        }
+        // Ação tomada ao arrastar para a
+        // direita: chamada da função de edição
+        // de tarefa do adapter que abre o
+        // fragmento de edição de tarefa
+        else {
             tarefaAdapter.editTarefaByIndex(tarefaIndex);
         }
     }
 
+    // Função responsável pelo comportamento
+    // da View de tarefa quando ela é arrastada
     @Override
     public void onChildDraw(
-            Canvas canvas,
-            RecyclerView recyclerView,
-            RecyclerView.ViewHolder viewHolder,
+            @NonNull Canvas canvas,
+            @NonNull RecyclerView recyclerView,
+            @NonNull RecyclerView.ViewHolder viewHolder,
             float dX,
             float dY,
             int actionState,
@@ -61,24 +79,45 @@ public class TarefaRecyclerItemTouchHelper extends ItemTouchHelper.SimpleCallbac
     ) {
         super.onChildDraw(canvas, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
 
+        // Declaração de recursos utilizados
+        // na estilização dos elementos em
+        // tela durante a ação
         Drawable swipeIcon;
         ColorDrawable background;
 
+        // Recuperação da view em qestão
         View itemView = viewHolder.itemView;
         int backgroundCornerOffset = 20;
 
+        // Deslocamento horizontal causado
+        // pelo usuário é maior que zero, se
+        // sim: exibição de um ícone de edição
+        // com a cor principal como fundo
         if (dX > 0) {
-            swipeIcon = ContextCompat.getDrawable(tarefaAdapter.getContext(), R.drawable.edit_icon);
-            background = new ColorDrawable(ContextCompat.getColor(tarefaAdapter.getContext(), R.color.colorPrimary));
-        } else {
-            swipeIcon = ContextCompat.getDrawable(tarefaAdapter.getContext(), R.drawable.delete_icon);
+            swipeIcon = ContextCompat.getDrawable(
+                    tarefaAdapter.getContext(), R.drawable.edit_icon);
+            background = new ColorDrawable(
+                    ContextCompat.getColor(tarefaAdapter.getContext(), R.color.colorPrimary));
+        }
+        // Caso contrário é exibido um ícone
+        // de deleção do item em questão
+        // com o fundo em vermelho
+        else {
+            swipeIcon = ContextCompat.getDrawable(
+                    tarefaAdapter.getContext(), R.drawable.delete_icon);
             background = new ColorDrawable(Color.RED);
         }
 
+        // Definição de variáveis de posicionamento do ícone
         int swipeIconMargin = (itemView.getHeight() - swipeIcon.getIntrinsicHeight()) / 2;
-        int swipeIconTop = itemView.getTop() + (itemView.getHeight() - swipeIcon.getIntrinsicHeight()) / 2;
+        int swipeIconTop = itemView.getTop() +
+                (itemView.getHeight() - swipeIcon.getIntrinsicHeight()) / 2;
         int swipeIconBottom = swipeIconTop + swipeIcon.getIntrinsicHeight();
 
+        // Deslocamento horizontal causado
+        // pelo usuário é maior que zero, se
+        // sim: definição de determinada aparência,
+        // posicionamento e limites para ícone/fundo
         if (dX > 0) {
             int swipeIconLeft = itemView.getLeft() + swipeIconMargin;
             int swipeIconRight = itemView.getLeft() + swipeIconMargin + swipeIcon.getIntrinsicHeight();
@@ -89,7 +128,12 @@ public class TarefaRecyclerItemTouchHelper extends ItemTouchHelper.SimpleCallbac
                     itemView.getTop(),
                     itemView.getLeft() + ((int) dX) + backgroundCornerOffset,
                     itemView.getBottom());
-        } else if (dX < 0) {
+        }
+        // Deslocamento horizontal causado
+        // pelo usuário é menor que zero, se
+        // sim: definição de outra aparência,
+        // posicionamento e limites para ícone/fundo
+        else if (dX < 0) {
             int swipeIconLeft = itemView.getRight() - swipeIconMargin - swipeIcon.getIntrinsicWidth();
             int swipeIconRight = itemView.getRight() - swipeIconMargin;
 
@@ -99,10 +143,17 @@ public class TarefaRecyclerItemTouchHelper extends ItemTouchHelper.SimpleCallbac
                     itemView.getTop(),
                     itemView.getRight(),
                     itemView.getBottom());
-        } else {
+        }
+        // Deslocamento horizontal causado
+        // pelo usuário é igual a zero, se
+        // sim: definição do fundo
+        // com dimensões nulas
+        else {
             background.setBounds(0, 0, 0, 0);
         }
 
+        // Desenho do ícone e fundo
+        // configurados na tela
         background.draw(canvas);
         swipeIcon.draw(canvas);
     }
